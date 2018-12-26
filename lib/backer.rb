84544cc2ea80f4480b68ff1ddda61d2f5062d0de
Brian Rosengarten <barosengarten@gmail.com>
2018-12-26 05:56:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,8 +9,9 @@ end
 
 def back_project(project)
   self.backed_projects << project
-  proj = Project.new(project)
-  proj.add_backer(self)
+#  proj = Project.new(project)
+#  proj.add_backer(self)
+  Project.backers << self
 end
 
 end
