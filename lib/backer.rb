0068da01d564333ec51c48bf55b4035e4d46319c
Brian Rosengarten <barosengarten@gmail.com>
2018-12-26 05:26:14 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
 
-attr_accessor :backed_projects, :name, :backers
+attr_accessor :backed_projects, :name
 
 def initialize(name)
   @backed_projects = []
@@ -9,7 +9,7 @@ end
 
 def back_project(project)
   self.backed_projects << project
-  @backers << self.name
+  #@backers << self.name
 end
 
 end
